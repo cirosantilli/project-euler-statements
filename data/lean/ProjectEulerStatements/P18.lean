139e7a4cfdd9da11bda5480ae9ffc7396f4cfec8
Ciro Santilli <cirosantilli@proton.me>
2026-01-30 00:00:04 +0000

    P18

--- data/lean/ProjectEulerStatements/P18.lean
+++ data/lean/ProjectEulerStatements/P18.lean
@@ -1,6 +1,12 @@
 import Mathlib.Data.List.Basic
+import Mathlib.Tactic
 
 namespace ProjectEulerStatements.P18
+
+def Triangle (n : Nat) :=
+  { rows : List (List Nat) //
+      rows.length = n ∧
+        ∀ i (hi : i < rows.length), (rows.get ⟨i, hi⟩).length = i + 1 }
 
 def maxRow : List Nat -> List Nat -> List Nat
   | [], _ => []
@@ -14,30 +20,24 @@ def maxPath (tri : List (List Nat)) : Nat :=
   | r :: rs =>
       List.getD (List.foldl (fun acc row => maxRow row acc) r rs) 0 0
 
-def smallTriangle : List (List Nat) :=
-  [ [3], [7, 4], [2, 4, 6], [8, 5, 9, 3] ]
+def smallTriangleT : Triangle 4 := by
+  refine ⟨
+    ([
+        [3],
+        [7, 4],
+        [2, 4, 6],
+        [8, 5, 9, 3]
+      ] : List (List Nat)
+    ),
+    ?_
+  ⟩
+  decide
 
-def bigTriangle : List (List Nat) :=
-  [ [75],
-    [95, 64],
-    [17, 47, 82],
-    [18, 35, 87, 10],
-    [20, 4, 82, 47, 65],
-    [19, 1, 23, 75, 3, 34],
-    [88, 2, 77, 73, 7, 63, 67],
-    [99, 65, 4, 28, 6, 16, 70, 92],
-    [41, 41, 26, 56, 83, 40, 80, 70, 33],
-    [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
-    [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
-    [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
-    [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
-    [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
-    [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23] ]
+/-- Maxium path down a Triangle `tri` going left or right. -/
+def naive (tri : Triangle n) : Nat :=
+  maxPath tri.1
 
-def naive : Nat :=
-  maxPath bigTriangle
-
-example : maxPath smallTriangle = 23 := by
+example : maxPath smallTriangleT.1 = 23 := by
   native_decide
 
 end ProjectEulerStatements.P18
--- data/lean/ProjectEulerStatements/P67.lean
+++ data/lean/ProjectEulerStatements/P67.lean
@@ -1,26 +1,8 @@
-import Mathlib.Data.List.Basic
+import ProjectEulerStatements.P18
 
 namespace ProjectEulerStatements.P67
 
-def maxRow : List Nat -> List Nat -> List Nat
-  | [], _ => []
-  | _ :: _, [] => []
-  | x :: xs, y :: z :: zs => (x + Nat.max y z) :: maxRow xs (z :: zs)
-  | _, _ => []
-
-def maxPath (tri : List (List Nat)) : Nat :=
-  match tri.reverse with
-  | [] => 0
-  | r :: rs =>
-      List.getD (List.foldl (fun acc row => maxRow row acc) r rs) 0 0
-
-def naive (tri : List (List Nat)) : Nat :=
-  maxPath tri
-
-def smallTriangle : List (List Nat) :=
-  [ [3], [7, 4], [2, 4, 6], [8, 5, 9, 3] ]
-
-example : maxPath smallTriangle = 23 := by
-  native_decide
+def naive (tri : ProjectEulerStatements.P18.Triangle n) : Nat :=
+  ProjectEulerStatements.P18.naive tri
 
 end ProjectEulerStatements.P67
