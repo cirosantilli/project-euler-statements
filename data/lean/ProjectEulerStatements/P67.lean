838611eab9d7c01608221e9cfc4b4b34435e37c8
Ciro Santilli <cirosantilli@proton.me>
2026-06-05 01:00:00 +0000

    Improve P18

--- data/lean/ProjectEulerStatements/P67.lean
+++ data/lean/ProjectEulerStatements/P67.lean
@@ -2,7 +2,7 @@ import ProjectEulerStatements.P18
 
 namespace ProjectEulerStatements.P67
 
-def naive (tri : ProjectEulerStatements.P18.Triangle n) : Nat :=
+def naive (tri : ProjectEulerStatements.P18.Triangle) : Nat :=
   ProjectEulerStatements.P18.naive tri
 
 end ProjectEulerStatements.P67
